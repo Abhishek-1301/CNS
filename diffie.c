ee73158f984391dcbbd2098065451c4a0977f541
Abhishek Bheemarasetty <84447693+Abhishek-1301@users.noreply.github.com>
2023-06-06 00:55:18 +0530

    Update diffie.c

--- diffie.c
+++ diffie.c
@@ -9,12 +9,12 @@ G = 9;
 printf("The value of G : %d\n\n", G);
 a = 4;
 printf("The private key a for Alice : %d\n", a);
-x = pow(G, a) % P;
+x = fmod(pow(G, a) ,P);
 b = 3;
 printf("The private key b for Bob : %d\n\n", b);
-y = pow(G, b) % P;
-ka = pow(y, a) % P; // Secret key for Alice
-kb = power(x, b) % P; // Secret key for Bob
+y =fmod( pow(G, b), P);
+ka = fmod(pow(y, a), P); // Secret key for Alice
+kb = fmod(power(x, b), P); // Secret key for Bob
 printf("Secret key for the Alice is : %d\n", ka);
 printf("Secret Key for the Bob is : %d\n", kb);
 return 0;
