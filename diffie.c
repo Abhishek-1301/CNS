fdcc8170046d7599736b4b42422b44ff842e605d
Abhishek Bheemarasetty <84447693+Abhishek-1301@users.noreply.github.com>
2023-06-06 00:53:25 +0530

    Update diffie.c

--- diffie.c
+++ diffie.c
@@ -2,20 +2,20 @@
 #include<math.h>
 int main()
 {
-long long int P, G, x, a, y, b, ka, kb;
+int P, G, x, a, y, b, ka, kb;
 P = 23;
-printf("The value of P : %lld\n", P);
+printf("The value of P : %d\n", P);
 G = 9;
-printf("The value of G : %lld\n\n", G);
+printf("The value of G : %d\n\n", G);
 a = 4;
-printf("The private key a for Alice : %lld\n", a);
+printf("The private key a for Alice : %d\n", a);
 x = pow(G, a) % P;
 b = 3;
-printf("The private key b for Bob : %lld\n\n", b);
+printf("The private key b for Bob : %d\n\n", b);
 y = pow(G, b) % P;
 ka = pow(y, a) % P; // Secret key for Alice
 kb = power(x, b) % P; // Secret key for Bob
-printf("Secret key for the Alice is : %lld\n", ka);
-printf("Secret Key for the Bob is : %lld\n", kb);
+printf("Secret key for the Alice is : %d\n", ka);
+printf("Secret Key for the Bob is : %d\n", kb);
 return 0;
 }
